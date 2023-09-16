b825b1b40bbf587e5d78a35e60283f54f03f776e
steviejames <silviojava2@gmail.com>
2023-09-16 08:35:53 +0100

    Changed image import to img tag

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -3,7 +3,6 @@ import Link from "next/link";
 // Import react scroll
 import { Link as LinkScroll } from "react-scroll";
 import ButtonOutline from "../misc/ButtonOutline.";
-import Logo from "../../public/assets/logo.svg";
 
 const Header = () => {
   const [activeLink, setActiveLink] = useState(null);
@@ -23,7 +22,7 @@ const Header = () => {
       >
         <nav className="max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col py-3 sm:py-4">
           <div className="col-start-1 col-end-2 flex items-center">
-            <Logo className="h-8 w-auto" />
+            <img src="/assets/logo.svg" className="h-8 w-auto mb-6" />
           </div>
           <ul className="hidden lg:flex col-start-4 col-end-8 text-black-500  items-center">
             <LinkScroll
